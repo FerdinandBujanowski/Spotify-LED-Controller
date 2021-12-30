72d16eadf88a34b5ed6b66d0d903f32283dd50e5
FerdinandBujanowski <49587549+FerdinandBujanowski@users.noreply.github.com>
2021-12-30 20:11:00 +0100

    neue GUI Bedienelemente mit Spotify API verbunden

--- src/main/java/control/song/SongControl.java
+++ src/main/java/control/song/SongControl.java
@@ -2,55 +2,85 @@ package control.song;
 
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
 import com.wrapper.spotify.model_objects.miscellaneous.CurrentlyPlayingContext;
+import com.wrapper.spotify.model_objects.specification.Track;
 import com.wrapper.spotify.model_objects.specification.User;
 import com.wrapper.spotify.requests.data.player.GetInformationAboutUsersCurrentPlaybackRequest;
 import com.wrapper.spotify.requests.data.player.PauseUsersPlaybackRequest;
 import com.wrapper.spotify.requests.data.player.StartResumeUsersPlaybackRequest;
+import com.wrapper.spotify.requests.data.search.simplified.SearchTracksRequest;
 import com.wrapper.spotify.requests.data.users_profile.GetCurrentUsersProfileRequest;
 import control.spotify.SpotifyWebHandler;
 import org.apache.hc.core5.http.ParseException;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class SongControl {
 
     private SpotifyWebHandler spotifyWebHandler;
 
     private User lastConnectedUser;
-    private int songId;
+    private String songId;
     private boolean songSelected;
+
+    private Track[] lastSearchedSongList;
+    private Track selectedSong;
+
 
     public SongControl() {
         this.spotifyWebHandler = new SpotifyWebHandler();
 
-        this.songId = 0;
+        this.songId = "";
         this.songSelected = false;
+
+        this.lastSearchedSongList = new Track[] {};
+        this.selectedSong = null;
     }
 
-    public SongControl(int songID) {
-        this();
-        this.selectSong(songId);
-    }
 
-    public void connectToSpotify() {
+    public boolean connectToSpotify() {
         this.spotifyWebHandler.init();
         if(this.spotifyWebHandler.getSpotifyApi().getAccessToken() != null) {
             GetCurrentUsersProfileRequest currentUsersProfileRequest = this.spotifyWebHandler.getSpotifyApi().getCurrentUsersProfile().build();
             try {
                 this.lastConnectedUser = currentUsersProfileRequest.execute();
+                return true;
             } catch (IOException | SpotifyWebApiException | ParseException e) {
                 e.printStackTrace();
             }
         }
+        return false;
     }
 
     public String getLastConnectedUserName() {
         return lastConnectedUser.getDisplayName();
     }
 
-    public void selectSong(int songId) {
-        this.songId = songId;
+    public String[] searchSongByName(String input) {
+        SearchTracksRequest searchTracksRequest = this.spotifyWebHandler.getSpotifyApi().searchTracks(input).build();
+        try {
+            Track[] tracks = searchTracksRequest.execute().getItems();
+            this.lastSearchedSongList = tracks;
+            return Arrays.copyOf(Arrays.stream(tracks).map(e -> e.getName() + " by " + e.getArtists()[0].getName()).toArray(), tracks.length, String[].class);
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            e.printStackTrace();
+        }
+        return new String[] {};
+    }
+
+    public void selectSongBySearchedList(int selectedListIndex) {
+        this.selectedSong = this.lastSearchedSongList[selectedListIndex];
+        this.songId = this.selectedSong.getId();
         this.songSelected = true;
+    }
+
+    public String getImageURL() {
+        try {
+            return this.selectedSong.getAlbum().getImages()[0].getUrl();
+        } catch(NullPointerException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public CurrentlyPlayingContext onGetCurrentPlayingTrack() {
--- src/main/java/gui/main_panels/player_panel/SpotifyPlayerPanel.java
+++ src/main/java/gui/main_panels/player_panel/SpotifyPlayerPanel.java
@@ -2,43 +2,62 @@ package gui.main_panels.player_panel;
 
 import control.song.SongControl;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.URL;
 
 public class SpotifyPlayerPanel extends JPanel {
 
     private SongControl songControl;
 
-    private JLabel connectedLabel;
+    private final JLabel connectedLabel;
+    private JButton connectToSpotifyButton;
+
+    private final JTextField searchSongField;
+    private final JButton searchSongButton;
+    private final JComboBox<String> selectSongBox;
+    private final JButton selectSongButton;
+
+    private JLabel songImageLabel;
 
     public SpotifyPlayerPanel(SongControl songControl, Dimension windowDimension) {
         super(null);
 
-        this.songControl = songControl;
-
         this.setOpaque(true);
         this.setBackground(new Color(24, 24, 24));
 
+        this.songControl = songControl;
+
+        //Connected Label
         this.connectedLabel = new JLabel("", SwingConstants.CENTER);
         this.connectedLabel.setForeground(Color.WHITE);
         this.connectedLabel.setBounds((windowDimension.width - 200) / 2, 5, 200, 20);
-        //this.add(this.songPlayerPanel);
-        //this.songPlayerPanel.setBounds(100, 100, 500, 500);
 
-        JButton connectToSpotifyButton = new JButton("Connect to your Spotify");
-        connectToSpotifyButton.addActionListener(new ActionListener() {
+        this.add(this.connectedLabel);
+
+        //Connect To Spotify Button
+        this.connectToSpotifyButton = new JButton("Connect to your Spotify");
+        this.connectToSpotifyButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                songControl.connectToSpotify();
+                if(songControl.connectToSpotify()) {
+                    searchSongField.setEnabled(true);
+                    searchSongButton.setEnabled(true);
+                    selectSongBox.setEnabled(true);
+                    selectSongButton.setEnabled(true);
+                }
                 repaint();
             }
         });
-        connectToSpotifyButton.setBounds((windowDimension.width - 200) / 2, 30, 200, 20);
+        this.connectToSpotifyButton.setBounds((windowDimension.width - 200) / 2, 30, 200, 20);
 
-        JButton clickMeButton = new JButton("Click Me!");
+        this.add(this.connectToSpotifyButton);
+
         /**
         clickMeButton.addActionListener(new ActionListener() {
             @Override
@@ -56,11 +75,68 @@ public class SpotifyPlayerPanel extends JPanel {
             }
         });
         **/
-        clickMeButton.setBounds(10, 10, 40, 10);
 
-        this.add(this.connectedLabel);
-        this.add(connectToSpotifyButton);
-        //this.add(clickMeButton);
+        //Search Song Field
+        this.searchSongField = new JTextField();
+        this.searchSongField.setBounds((windowDimension.width / 4) - 150, 30, 200, 20);
+        this.searchSongField.setEnabled(false);
+
+        this.add(this.searchSongField);
+
+        //Search Song Button
+        this.searchSongButton = new JButton("Search");
+        this.searchSongButton.setBounds((windowDimension.width / 4) + 51, 30, 100, 20);
+        this.searchSongButton.setEnabled(false);
+
+        this.searchSongButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String[] trackList = songControl.searchSongByName(searchSongField.getText());
+                DefaultComboBoxModel<String> newModel = new DefaultComboBoxModel<>(trackList);
+                selectSongBox.setModel(newModel);
+            }
+        });
+
+        this.add(searchSongButton);
+
+        //Select Song Box
+        this.selectSongBox = new JComboBox<>();
+        this.selectSongBox.setBounds((windowDimension.width / 4) - 150, 55, 200, 20);
+        this.selectSongBox.setEnabled(false);
+
+        this.add(selectSongBox);
+
+        //Select Song Button
+        this.selectSongButton = new JButton("Select");
+        this.selectSongButton.setBounds((windowDimension.width / 4) + 51, 55, 100, 20);
+        this.selectSongButton.setEnabled(false);
+
+        this.selectSongButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                songControl.selectSongBySearchedList(selectSongBox.getSelectedIndex());
+
+                String imageURL = songControl.getImageURL();
+                if(imageURL != null) {
+                    BufferedImage image = null;
+                    try {
+                        image = ImageIO.read(new URL(imageURL));
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                    songImageLabel.setIcon(new ImageIcon(getScaledImage(image, 300, 300)));
+                    repaint();
+                }
+            }
+        });
+
+        this.add(selectSongButton);
+
+        //Song Image Label
+        this.songImageLabel = new JLabel();
+        this.songImageLabel.setBounds((windowDimension.width - 300) / 2, 100, 300, 300);
+
+        this.add(this.songImageLabel);
     }
 
     @Override
@@ -71,5 +147,16 @@ public class SpotifyPlayerPanel extends JPanel {
         } catch (NullPointerException e) {
             this.connectedLabel.setText("[ not connected ]");
         }
+    }
+
+    public static Image getScaledImage(Image srcImg, int w, int h) {
+        BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = resizedImg.createGraphics();
+
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2.drawImage(srcImg, 0, 0, w, h, null);
+        g2.dispose();
+
+        return resizedImg;
     }
 }
